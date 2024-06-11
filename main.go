b218edf1c45ccda7110f85c45c4e29ab1f4f0aaa
markjkaem <markteek@gmail.com>
2024-06-12 01:44:31 +0200

    ff

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
-import (
-	"test/learning"
-)
+import "fmt"
+
+//When
+//1. When we need to update state
+
+type User struct {
+	name  string
+	age   uint8
+	email string
+}
+
+func (u User) Email() string {
+	return u.email
+}
+
+func (u *User) updateEmail(email string) {
+	u.email = email
+}
+
+func Email(u *User) string {
+	return u.email
+}
 
 func main() {
-	// learning.ScraperExec()
-	learning.MainCtx()
+	user := User{
+		email: "mark.teekens@outlook.com",
+	}
+	user.updateEmail("foobar@gmail.com")
+	fmt.Println(user.Email())
 }
